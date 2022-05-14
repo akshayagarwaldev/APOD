218dff97df963f0533baaa0a6f34b47d18a4c9e0
Akshay Agarwal <akshayagarwalak@gmail.com>
2022-05-14 20:39:51 +0530

    Added UI elements and outlets

---
+++ APOD/APOD/View/APODViewController.swift
@@ -0,0 +1,23 @@
+//
+//  APODViewController.swift
+//  APOD
+//
+//  Created by aksagarw on 14/05/22.
+//
+
+import UIKit
+
+class APODViewController: UIViewController {
+
+    @IBOutlet weak var astronomyImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var explanationLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- APOD/APOD/View/HomeViewController.swift
+++ APOD/APOD/View/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  APOD
 //
 //  Created by aksagarw on 14/05/22.
@@ -7,8 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBAction func APODButtonTapped(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
